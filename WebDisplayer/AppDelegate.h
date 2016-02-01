94d63f1dd05cec1fc0d39797e1ce1b13430165f3
wentianqi7 <wentianqi7@gmail.com>
2016-02-01 10:55:26 -0500

    Initial Commit

---
+++ WebDisplayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebDisplayer
+//
+//  Created by Tianqi Wen on 2/1/16.
+//  Copyright © 2016 Tianqi Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebDisplayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebDisplayer
+//
+//  Created by Tianqi Wen on 2/1/16.
+//  Copyright © 2016 Tianqi Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
