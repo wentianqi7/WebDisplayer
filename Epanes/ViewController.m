163ec5bce13291cd4bd158ee8bbe6b2143a5a422
bilibili2333 <rainyyang5@gmail.com>
2016-02-22 00:45:42 -0500

    move web view content to index.html

--- Epanes/ViewController.m
+++ Epanes/ViewController.m
@@ -18,9 +18,23 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	
-	NSLog(@"web view display url = %@", _destStr);
+	// get url from default.json
+	NSString *webDir = @"http://epanes.math.cmu.edu/json/";
+	NSString *filename = @"default.json";
+	NSString *destStr;
 	
-	NSURL *destUrl = [NSURL URLWithString:[_destStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+	NSMutableArray *result = [self getJsonContent:webDir filename:filename];
+	for (NSMutableDictionary *dic in result) {
+		NSString *string = dic[@"url"];
+		if (string) {
+			// valid project field found
+			destStr = [self processString:string];
+			NSLog(@"%@", destStr);
+		}
+	}
+	
+	// set content of webview to the url
+	NSURL *destUrl = [NSURL URLWithString:[destStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
 	NSURLRequest *urlRequest = [NSURLRequest requestWithURL:destUrl];
 	[_webView loadRequest:urlRequest];
 	
@@ -35,15 +49,29 @@
 }
 
 - (IBAction)titleButtonClick:(id)sender {
-	/*
-	NSString *destStr = [NSString stringWithFormat:@"http://www.google.com"];
-	NSURL *destUrl = [NSURL URLWithString:destStr];
-	NSURLRequest *urlRequest = [NSURLRequest requestWithURL:destUrl];
-	[_webView loadRequest:urlRequest];
-	 */
 	CollectionViewController *viewController = [self.storyboard instantiateViewControllerWithIdentifier:@"CollectionViewController"];
 	[self presentModalViewController:viewController animated:YES];
 	
+}
+
+// get json content from given website with filename
+- (NSMutableArray *)getJsonContent:(NSString *)webDir filename:(NSString *)filename {
+	NSHTTPURLResponse *response = nil;
+	NSString *sourceStr = [NSString stringWithFormat:[webDir stringByAppendingString:filename]];
+	NSURL *sourceUrl = [NSURL URLWithString:[sourceStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+	
+	NSURLRequest *request = [[NSURLRequest alloc] initWithURL:sourceUrl
+												  cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60.0];
+	NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
+	NSMutableArray *result  = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:nil];
+	return result;
+}
+
+// encode the input string
+- (NSString *)processString:(NSString *)inputStr {
+	NSData *data = [inputStr dataUsingEncoding:NSUTF8StringEncoding];
+	NSString *outputStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+	return outputStr;
 }
 
 
